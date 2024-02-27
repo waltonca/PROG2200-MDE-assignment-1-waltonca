6cbae24250eba2bbcb77f57ab9fca616293f9cf5
Junxiao <W0486229@campus.nscc.ca>
2024-02-26 21:36:01 -0400

    Done basic client server connection

--- Assignment1ChatProgram/Program.cs
+++ Assignment1ChatProgram/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,16 +20,81 @@ namespace Assignment1ChatProgram
             if(args.Contains("-server")) {
                 Console.WriteLine("Server");
                 // Code to run as a server
-                Server server = new Server();
+                //Server server = new Server();
+                TcpListener server = null;
+                try
+                {
+                    // Set the TcpListener on port 13000.
+                    Int32 port = 13000;
+                    IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                    // TcpListener server = new TcpListener(port);
+                    server = new TcpListener(localAddr, port);
+
+                    // Start listening for client requests.
+                    server.Start();
+
+                    // Buffer for reading data
+                    Byte[] bytes = new Byte[256];
+                    String data = null;
+
+                    // Enter the listening loop.
+                    while (true)
+                    {
+                        Console.Write("Waiting for a connection... ");
+
+                        // Perform a blocking call to accept requests.
+                        // You could also use server.AcceptSocket() here.
+                        TcpClient client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
+
+                        data = null;
+
+                        // Get a stream object for reading and writing
+                        NetworkStream stream = client.GetStream();
+
+                        int i;
+
+                        // Loop to receive all the data sent by the client.
+                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            // Translate data bytes to a ASCII string.
+                            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                            Console.WriteLine("Received: {0}", data);
+
+                            // Process the data sent by the client.
+                            data = data.ToUpper();
+
+                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                            // Send back a response.
+                            stream.Write(msg, 0, msg.Length);
+                            Console.WriteLine("Sent: {0}", data);
+                        }
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+                finally
+                {
+                    server.Stop();
+                }
+
+                Console.WriteLine("\nHit enter to continue...");
+                Console.Read();
             }
             else
             {
                 Console.WriteLine("Client");
                 // Code to run as a client
-                Client client = new Client();
+                //Client client = new Client();
+                Connect("127.0.0.1", "Hi Server");
+
             }
 
-            
+            /*
             
                 while (true)
                 {
@@ -62,7 +129,63 @@ namespace Assignment1ChatProgram
                 }
                 
 
+            */
+        }
 
+        //The Connect Method:
+        static void Connect(String server, String message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 13000;
+
+                // Prefer a using declaration to ensure the instance is Disposed later.
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the server response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Explicit close is not necessary since TcpClient.Dispose() will be
+                // called automatically.
+                // stream.Close();
+                // client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
         }
     }
 }
