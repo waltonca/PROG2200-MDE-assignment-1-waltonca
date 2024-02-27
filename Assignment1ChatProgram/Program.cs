0abfc52fbf4c3dce2ec2acb742f9ad04ad07f8bb
Junxiao <W0486229@campus.nscc.ca>
2024-02-26 21:27:04 -0400

    Add project files.

---
+++ Assignment1ChatProgram/Program.cs
@@ -0,0 +1,68 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment1ChatProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dectect if the program is running as a server or client
+            // Use the -server argument to run as a server
+            // Use the no argument to run as a client
+            if(args.Contains("-server")) {
+                Console.WriteLine("Server");
+                // Code to run as a server
+                Server server = new Server();
+            }
+            else
+            {
+                Console.WriteLine("Client");
+                // Code to run as a client
+                Client client = new Client();
+            }
+
+            
+            
+                while (true)
+                {
+                    //Part 1
+                    //Run as Client vs Server
+                    Console.WriteLine("Listening for messages");
+
+                    if (Console.KeyAvailable)
+                    {
+                        //User input mode: when user press "I" key.
+                        //Console.ReadLine();
+                        ConsoleKeyInfo userKey = Console.ReadKey();
+
+                        if (userKey.Key == ConsoleKey.I)
+                        {
+                            Console.Write("'I' is PRESSED >>");
+                            Thread.Sleep(2000);
+                            Console.ReadLine();
+
+                        }
+                        else if (userKey.Key == ConsoleKey.Escape) 
+                        { 
+                            break; // Close the client node object.
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You typed {userKey.Key}");
+                            Thread.Sleep(2000);
+                        }
+                    
+                    }
+                }
+                
+
+
+        }
+    }
+}
