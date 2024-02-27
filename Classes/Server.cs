a37e77f0eec67a3134b01efd35540195f7122d24
Junxiao <W0486229@campus.nscc.ca>
2024-02-26 22:08:32 -0400

    Moved Server code into Server class

--- Classes/Server.cs
+++ Classes/Server.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,61 @@ namespace Classes
 {
     public class Server
     {
+        private TcpListener server = null;
+        public Server()
+        {
+            // Set the TcpListener on port 13000.
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(localAddr, port);
+        }
+        public void Start()
+        {
+            // Start listening for client requests.
+            server.Start();
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
 
+            // Enter the listening loop.
+            while (true)
+            {
+                Console.WriteLine("Waiting for any Client connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connection established with Client!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+            }
+        }
+        public void Stop()
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
     }
 }
